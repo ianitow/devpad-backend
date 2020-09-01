3dec9e5bfeb99e0b68021e5d470164ee81834bce
ianito <iaan.developer@gmail.com>
2020-08-31 23:21:44 -0300

    tag sending doc created in endpoint /tag post

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,14 +9,15 @@ export default async (req, res, next) => {
   }
 
   const [, token] = authHeader.split(" ");
-
   try {
     const decoded = await promisify(jwt.verify)(token, "secret");
-
+    console.log(decoded);
+    console.log("aq");
     req.user_id = decoded.id;
 
     return next();
   } catch (err) {
+    console.log("errpr:", err);
     return res.status(401).send({ error: "Token invalid" });
   }
 };
