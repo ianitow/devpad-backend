f2c3883ad323c1b7bc0c50b48b9cef97fbddbea3
Lavínia Melo <lavininhamelo@hotmail.com>
2020-09-03 23:23:47 -0400

    fet: note remove function

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -2,6 +2,7 @@ import Note from '../models/Note';
 
 class NoteController {
   async index(req, res) {
+    console.log(req.user_id);
     const note = await Note.find({
       author_id: req.user_id,
     }).populate('tags');
@@ -16,49 +17,67 @@ class NoteController {
       const note = await Note.findById(id);
 
       if (!note) {
-        return res.status(400).json({ error: 'Anotação não encontrada!' });
+        return res.status(400).json({ error: 'Note was not found!' });
       }
 
       if (note.author_id != user_id) {
-        return res.status(400).json({ error: 'Usuário inválido!' });
+        return res.status(400).json({ error: 'Invalid user!' });
       }
 
-      return res.json(note);
+      return res.status(200).json(note);
     } catch (err) {
-      return res.status(400).json({ error: 'Erro ao recuperar dados!' });
+      return res.status(400).json({ error: 'Error retrieving data!' });
     }
   }
 
   async create(req, res) {
-    let { title, author_id, tags, content, isRedirect, url } = req.body;
-
+    let { title, tags, content, isRedirect, url } = req.body;
     try {
-      if (author_id != req.user_id) {
-        return res.status(400).json({ error: 'Usuário inválido!' });
-      }
-
       let note = {
         title,
-        author_id,
+        author_id: req.user_id,
         tags,
         isRedirect,
         content,
         url,
       };
 
-      //Garantir que não seja ambos os casos
       if (isRedirect) {
         note.content = [];
       } else note.url = null;
 
-      const notee = await Note.create(note);
-      return res.json({ notee });
+      const note_data = await Note.create(note);
+      return res.json(note_data);
     } catch (err) {
-      return res.status(400).json({ error: 'Erro ao cadastrar anotação' });
+      return res.status(400).json({ error: 'Error registering note' });
     }
   }
   async update(req, res) {}
-  async store(req, res) {}
+
+  async delete(req, res) {
+    const { user_id } = req;
+    const { id } = req.params;
+
+    try {
+      const note = await Note.findById(id);
+
+      if (!note) {
+        return res.status(400).json({ error: 'Note was not found!' });
+      }
+
+      if (note.author_id != user_id) {
+        return res.status(400).json({ error: 'Invalid user!' });
+      }
+
+      await note.remove();
+
+      return res
+        .status(200)
+        .json({ message: 'Note annotation has been deleted!' });
+    } catch (err) {
+      return res.status(400).json({ error: 'Error deleting data!' });
+    }
+  }
 }
 
 export default new NoteController();
